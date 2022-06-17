95ecd82a683506b5c290c24e1a301dceaacdaabb
abdullahyildiz111 <abdullahyildiz111@gmail.com>
2022-06-18 01:12:52 +0300

    jwt

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,13 +6,13 @@ import {
   getUsers,
 } from "../controllers/user.js";
 //import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req,res,next)=>{
-  res.send("hello user, you are logged in")
-})
+// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+//   res.send("hello user, you are logged in")
+// })
 
 // router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
 //   res.send("hello user, you are logged in and you can delete your account")
@@ -23,15 +23,15 @@ router.get("/checkauthentication", verifyToken, (req,res,next)=>{
 // })
 
 //UPDATE
-router.put("/:id",  updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 //DELETE
-router.delete("/:id",  deleteUser);
+router.delete("/:id", verifyUser,  deleteUser);
 
 //GET
-router.get("/:id",  getUser);
+router.get("/:id",verifyUser, getUser);
 
 //GET ALL
-router.get("/",  getUsers);
+router.get("/",verifyAdmin, getUsers);
 
 export default router;
